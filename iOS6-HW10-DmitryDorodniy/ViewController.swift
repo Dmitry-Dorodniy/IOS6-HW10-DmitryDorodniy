dba0a865700b18b5c87de0dd2a5d5288f694d7d0
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-05-02 07:34:35 +0300

    bug-fix: delete all comments

--- iOS6-HW10-DmitryDorodniy/ViewController.swift
+++ iOS6-HW10-DmitryDorodniy/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  iOS6-HW10-DmitryDorodniy
-//
-//  Created by Dmitry Dorodniy on 02.05.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     func setupView() {
